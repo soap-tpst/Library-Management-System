4e1ec85aa3dec323b08f4418ffe1577d812d09d1
soap-tpst <83483731+soap-tpst@users.noreply.github.com>
2024-12-04 10:34:40 +0800

    Added: Template test for server

--- server/src/test/kotlin/ApplicationTest.kt
+++ server/src/test/kotlin/ApplicationTest.kt
@@ -1,6 +1,19 @@
+import com.soap.libms.module
+import io.ktor.client.request.get
+import io.ktor.client.statement.bodyAsText
+import io.ktor.http.HttpStatusCode
+import io.ktor.server.testing.testApplication
 import org.junit.Test
+import kotlin.test.assertEquals
 
 class ApplicationTest {
-
-
+    @Test
+    fun testRoot() = testApplication {
+        application {
+            module()
+        }
+        val response = client.get("/")
+        assertEquals(HttpStatusCode.OK, response.status)
+        assertEquals("Ktor", response.bodyAsText())
+    }
 }
